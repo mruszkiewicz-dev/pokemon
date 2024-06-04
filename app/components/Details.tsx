0e7a54722c80b3a9ad3de1e21a0c315d0a922810
mruszkiewicz <mruszkiewicz.dev@gmail.com>
2024-06-04 20:32:45 +0200

    styled

--- app/components/Details.tsx
+++ app/components/Details.tsx
@@ -8,17 +8,23 @@ export const Details = ({ pokemon }: any) => {
     return <p>Loading...</p>;
   }
 
-  const imageUrl = pokemon.sprites?.other?.dream_world?.front_default || pokemon.sprites?.other?.home?.front_default;
+  const imageUrl =
+    pokemon.sprites?.other?.dream_world?.front_default ||
+    pokemon.sprites?.other?.home?.front_default;
 
   return (
     <BG>
-      <div>
-        <p>{pokemon.name}</p>
-        {imageUrl ? <Image src={imageUrl} alt={pokemon.name} width={200} height={200} /> : <p>No image available</p>}
-        {pokemon.types.map((typ: any, i: any) => (
-          <p key={i}>{typ.type.name}</p>
-        ))}
+      <div className="absolute inset-x-1/3 bottom-1/4		 	 h-96 w-1/5 size-full   bg-red-500 rounded-xl">
+        {imageUrl ? (
+          <Image src={imageUrl} alt={pokemon.name} width={200} height={200} />
+        ) : (
+          <p>No image available</p>
+        )}
       </div>
+      <p>{pokemon.name}</p>
+      {pokemon.types.map((typ: any, i: any) => (
+        <p key={i}>{typ.type.name}</p>
+      ))}
     </BG>
   );
 };
